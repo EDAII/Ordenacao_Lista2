bb87e475ac7c9e7c3a39fe6acbe9a9108c6021ca
joaovitorml <joao.lemos4000@gmail.com>
2018-03-29 08:52:27 -0300

    Created main.

--- lista2.cpp
+++ lista2.cpp
@@ -1,6 +1,20 @@
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <bits/stdc++.h>
 
 using namespace std;
+
+double calcula_tempo(const struct rusage *b, const struct rusage *a) {
+    if (b == NULL || a == NULL)
+        return 0;
+    else
+        return ((((a->ru_utime.tv_sec * 1000000 + a->ru_utime.tv_usec) -
+                 (b->ru_utime.tv_sec * 1000000 + b->ru_utime.tv_usec)) +
+                ((a->ru_stime.tv_sec * 1000000 + a->ru_stime.tv_usec) -
+                 (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec)))
+                / 1000000.0);
+}
+
 
 void insertionSort(int original[], int length) {
 	int i, j, atual;
@@ -39,6 +53,54 @@ void shellSort(int *vet, int size) {
 }
 
 int main(int argc, char const *argv[]) {
-  /* code */
+   struct rusage tempo_inicial, tempo_final;
+	 double tempo,tempo2;
+	 int tam;
+	 cout << "Insira o tamanho do vetor: ";
+	 cin >> tam;
+	 int vetor[tam],vetor2[tam];
+	 for (int i=1; i < tam; i++){
+    vetor[i] = rand()%10000;
+  }
+	for (int i=1; i < tam; i++){
+	 vetor2[i] = rand()%10000;
+ }
+
+ if (tam <= 20){
+    for (size_t i = 0; i < tam; i++)
+      cout << vetor[i] << " ";
+  }
+	cout << endl;
+	if (tam <= 20){
+    for (size_t i = 0; i < tam; i++)
+      cout << vetor2[i] << " ";
+  }
+	cout << endl;
+	getrusage(RUSAGE_SELF, &tempo_inicial);
+		shellSort(vetor,tam);
+	getrusage(RUSAGE_SELF, &tempo_final);
+	tempo = calcula_tempo(&tempo_inicial, &tempo_final);
+
+	if (tam <= 20){
+	    for (size_t i = 0; i < tam; i++)
+	      cout << vetor[i] << " ";
+	  }
+	cout << endl;
+
+	getrusage(RUSAGE_SELF, &tempo_inicial);
+		insertionSort(vetor2,tam);
+	getrusage(RUSAGE_SELF, &tempo_final);
+	tempo2 = calcula_tempo(&tempo_inicial, &tempo_final);
+
+	if (tam <= 20){
+	    for (size_t i = 0; i < tam; i++)
+	      cout << vetor2[i] << " ";
+	  }
+
+	cout << endl;
+
+  printf("Tempo Shell Sort: %lf\n",tempo);
+  printf("Tempo Insertion Sort: %lf\n",tempo2);
+
   return 0;
 }
